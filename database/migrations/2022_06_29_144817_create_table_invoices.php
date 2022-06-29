5750bb0c8a9ef4f949f1ed69af7e63e415a2e606
Kaisha Satrio <bdamnboyz.09@gmail.com>
2022-06-29 23:03:41 +0700

    update 290622

---
+++ database/migrations/2022_06_29_144817_create_table_invoices.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->bigIncrements('invoice_id');
+            $table->char('invoice_code', 36);
+            $table->char('airwaybill_id', 36);
+            $table->date('invoice_date');
+            $table->text('invoice_information');
+            $table->decimal('invoice_amount', $precision = 10, $scale = 2)->default(0);
+            $table->decimal('invoice_tax', $precision = 10, $scale = 2)->default(0);
+            $table->decimal('invoice_total', $precision = 10, $scale = 2)->default(0);
+            $table->integer('invoice_status')->default(0);
+            $table->char('created_by', 36)->nullable();
+            $table->char('modified_by', 36)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
